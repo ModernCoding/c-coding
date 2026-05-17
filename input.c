4b61267638e741f453c45d28d983daa0ad54edfe
ccc.444 <contact@coding-crafting-consulting.com>
2026-05-17 16:55:38 +0700

    input

---
+++ input.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+  int age = 0;
+  float gpa = 0.0f;
+  char grade = '\0';
+  char name[30] = "";
+
+  printf("Enter your age: ");
+  scanf("%d", &age);
+
+  printf("Enter your GPA: ");
+  scanf("%f", &gpa);
+
+  printf("Enter your grade: ");
+  scanf(" %c", &grade);
+
+  getchar(); // Consume the newline character left by previous scanf
+  printf("Enter your name: ");
+  fgets(name, sizeof(name), stdin);
+
+  // Remove the newline character from the name string
+  name[strcspn(name, "\n")] = '\0';
+
+  printf("%s\n", name);
+  printf("%d\n", age);
+  printf("%f\n", gpa);
+  printf("%c\n", grade);
+
+  return 0;
+}

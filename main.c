78d8f55db4066f90ae5640ad07810bb7eaee4b98
ccc.444 <contact@coding-crafting-consulting.com>
2026-05-17 15:29:06 +0700

    phi

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <stdio.h>
 
 int main ()
@@ -8,5 +9,18 @@ int main ()
   int age = 25;
   printf ("\n\n");
   printf ("I am %d years old.", age);
+
+  long double phi = (1.0L + sqrtl(5.0L)) / 2.0L;
+  printf ("\n\n");
+  printf ("The value of phi is: %.25Lf", phi);
+
+  long double pi = 4.0L * atanl(1.0L);
+  printf ("\n\n");
+  printf ("The value of pi is: %.25Lf", pi);
+
+  // long double phi2 = 2.0L * sinl(54.0L * pi / 180.0L);
+  // printf ("\n\n");
+  // printf ("The value of phi2 is: %.25Lf", phi2);
+
   return 0;
 }

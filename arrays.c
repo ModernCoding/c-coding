445ce8f5f08266b841b3ae48bbcd9dec4f5ca662
ccc.444 <contact@coding-crafting-consulting.com>
2026-05-19 22:21:41 +0700

    arrays

--- arrays.c
+++ arrays.c
@@ -3,10 +3,20 @@
 
 int main () {
 
-  int numbers [] = { 0, 1 };
+  int numbers [] = { 4, 19 };
 
   printf("%d\n", numbers [ 0 ]);
   printf("%d\n", numbers [ 1 ]);
+
+  // sizeof === in bytes
+
+  printf ("%ld\n", sizeof (numbers));
+  printf ("%ld\n", sizeof (numbers [ 0 ]));
+
+  char fruits [] [ 10 ] = { "Apple", "Banana", "Coconut" };
+
+  printf ("%ld\n", sizeof (fruits));
+  printf ("%ld\n", sizeof (fruits [ 0 ]));
 
   return 0;
 

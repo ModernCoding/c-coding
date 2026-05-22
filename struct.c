df15adda98de2eb80b0eb02cae5e0862a4e35fc3
ccc.444 <contact@coding-crafting-consulting.com>
2026-05-22 19:30:04 +0700

    struct

---
+++ struct.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+struct Student {
+  char name [50];
+  int age;
+  float gpa;
+  bool isFullTime;
+};
+
+
+int main () {
+
+  struct Student student1 = { "Spongebob", 30, 2.5, true };
+
+  printf ("%s\n", student1.name);
+
+  return 0;
+
+}
